ca635de2784b3966de23ceba1ff162904bdebf3a
HamedAjaj <hamedajaj906@gmail.com>
2023-06-23 12:39:20 +0300

    Update database by Adding 2 tables EmployeeWokData and Hours

--- HRSystem/Infrastructure/Migrations/20230623082309_Second_Update_Db_Classes.Designer.cs
+++ HRSystem/Infrastructure/Migrations/20230623082309_Second_Update_Db_Classes.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(HRAppDbContext))]
-    partial class HRAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230623082309_Second_Update_Db_Classes")]
+    partial class Second_Update_Db_Classes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
